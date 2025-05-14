d6f5660a8c6318b1c25b2160eba126aa0c609999
Michael Ramirez <michaelrp2000@gmail.com>
2025-05-14 14:59:46 -0500

    V.05

--- programa.js
+++ programa.js
@@ -1,7 +1,15 @@
 var map = L.map('map').setView([4.6279856,-74.0670738], 13);
-
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+
+// Abrir archivo GeoJSON
+// Buscar llaves de interes
+// SI el valor de la llave es un rreglo hacer un ciclo para recorrer elementos
+
+async function Cargarpuntos() {
+    var miArchivo= await fetch ("microondas.geojson")
+    
+}
